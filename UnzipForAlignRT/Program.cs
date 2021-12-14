09146d262a34c954f181932443253bab62e59f5b
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-12-14 14:52:09 -0800

    copied over filewater from the other unzip function

---
+++ UnzipForAlignRT/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using FellowOakDicom;
+
+namespace UnzipForAlignRT
+{
+    public class FileWatcher
+    {
+        private bool folder_changed;
+        public bool Folder_Changed
+        {
+            get
+            {
+                return folder_changed;
+            }
+            set
+            {
+                folder_changed = value;
+            }
+        }
+        public FileWatcher(string directory)
+        {
+            FileSystemWatcher file_watcher = new FileSystemWatcher(directory);
+            file_watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+            file_watcher.Changed += OnChanged;
+            file_watcher.EnableRaisingEvents = true;
+        }
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                folder_changed = false;
+                return;
+            }
+            folder_changed = true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
